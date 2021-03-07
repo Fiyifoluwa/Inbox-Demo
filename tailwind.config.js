226fc888ca33663fcd492bbf7ed5bce585c7cd06
Fiyi Oladimeji <fiyifoluwa@outlook.com>
2021-03-07 11:04:07 -0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addComponents }) {
+      const buttons = {
+        '.btn-red': {
+          padding: '0.5rem 1rem',
+          borderRadius: '0.25rem',
+          fontWeight: '600',
+          backgroundColor: '#e3342f',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#cc1f1a',
+          },
+        },
+      };
+
+      addComponents(buttons);
+    }),
+  ],
+};
